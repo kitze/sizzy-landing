f946fe75d13c231e1bf8793064de3a7e8264347a
Kitze <kristijan.mkd@gmail.com>
2026-06-20 11:19:53 +0000

    Remove dead Sizzy payment config

--- src/config/polar.ts
+++
@@ -1,3 +0,0 @@
-// ReleaseFlow checkout config
-export const SOTTO_CHECKOUT_URL = "https://releaseflow.net/kitze/sotto/checkout";
-export const SOTTO_PRODUCT_ID = "2e0ffad9-5be5-4f96-a7eb-cf81134d19d5";
